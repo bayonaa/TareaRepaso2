fe1e5422024b069d9a01750526e5e6bbe1b00b9c
bayonaa <mbayona03@gmail.com>
2024-03-25 19:58:05 +0100

    Implementación del tercer, cuarto y quinto test

--- src/es/iesmz/tests/VocalesTest.java
+++ src/es/iesmz/tests/VocalesTest.java
@@ -19,4 +19,19 @@ public class VocalesTest {
     public void test2() {
         assertEquals(8, Vocales.contarvocales ("Esto Es una cadenA"));
     }
+
+    @Test
+    public void test3() {
+        assertEquals(7, Vocales.contarvocales ("Java mola MogollOn"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(1, Vocales.contarvocales ("4h0r4 s0n num3r0s"));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(6, Vocales.contarvocales ("Cadena S1n Vocales"));
+    }
 }
