4a091ae9c1d865f8ec0a7ae1daada17fd9068cfb
bayonaa <mbayona03@gmail.com>
2024-03-25 19:52:18 +0100

    Método contarvocales implementado

--- src/es/iesmz/tests/Vocales.java
+++ src/es/iesmz/tests/Vocales.java
@@ -2,6 +2,15 @@ package es.iesmz.tests;
 
 public class Vocales {
     public static int contarvocales(String cadena) {
+        cadena = cadena.toLowerCase();
+        int cont = 0;
 
+        for (int i = 0; i < cadena.length(); i++) {
+            char letra = cadena.charAt(i);
+            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'á' || letra == 'é' || letra == 'í' || letra == 'ó' || letra == 'ú') {
+                cont++;
+            }
+        }
+        return cont;
     }
 }
