8c2e38c28862211bda90d7ef12ffd308957b9263
bayonaa <mbayona03@gmail.com>
2024-03-25 19:54:44 +0100

    Implementación de los imports, el BeforeEach y los dos primeros tests

--- src/es/iesmz/tests/VocalesTest.java
+++ src/es/iesmz/tests/VocalesTest.java
@@ -1,5 +1,22 @@
 package es.iesmz.tests;
 
-public class VocalesTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class VocalesTest {
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("EXECUTE PROVA");
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(4, Vocales.contarvocales ("Hola Mundo"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(8, Vocales.contarvocales ("Esto Es una cadenA"));
+    }
 }
